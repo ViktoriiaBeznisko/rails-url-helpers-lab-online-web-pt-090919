6ec51fc793ab811ae3cfc49bc61fbe8560d4e32f
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2020-02-03 04:49:56 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -9,7 +9,8 @@ class StudentsController < ApplicationController
   end
   
   def activate
-    @student = Student.find(params[:id])
+    set_student
+    # @student = Student.find(params[:id])
     @student.active = !@student.active
     @student.save
     redirect_to student_path(@student)
